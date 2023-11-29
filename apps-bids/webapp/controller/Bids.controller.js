0dbd84ed56071da306aa3d3a5952020ed7f31ba3
Shankar Idagichadi <shankar.idagichadi@lntinfotech.com>
2023-11-29 12:37:24 +0000

    new commit

--- apps-bids/webapp/controller/Bids.controller.js
+++ apps-bids/webapp/controller/Bids.controller.js
@@ -65,5 +65,16 @@ sap.ui.define([
                     MessageToast.show("No items selected for this RFQ");
                 }
             },
+            onRFQTypeSelected: function () {
+                var oType = sap.ui.getCore().byId("RBRFQType");
+                this.RFQType = oType.getSelectedButton().getText();
+    
+                this.onSelectType();
+                if (this._oDialog) {
+                    this._oDialog.close();
+                }
+    
+                this.onCreateRFQ();
+            },
         });
     });
