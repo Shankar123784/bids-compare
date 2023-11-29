eab2e42d52b09c3b4ce2fd09ea7c86710676c447
Shankar Idagichadi <shankar.idagichadi@lntinfotech.com>
2023-11-29 12:15:22 +0000

    29112023

--- apps-bids/webapp/controller/Bids.controller.js
+++ apps-bids/webapp/controller/Bids.controller.js
@@ -10,6 +10,21 @@ sap.ui.define([
         return Controller.extend("com.bids.appsbids.controller.Bids", {
             onInit: function () {
                 this.getView().byId("sematicPageId").destroyTitleHeading();
+            },
+            onSearchPackageCode: function (oEvent) {
+                var sValue = oEvent.getParameter("value");
+                var oFilter = new sap.ui.model.Filter("resAnalysisCostDescription", sap.ui.model.FilterOperator.Contains, sValue, false);
+                var oFilterCode = new sap.ui.model.Filter("packageCode", sap.ui.model.FilterOperator.Contains, sValue, false);
+                var oFilterPackageCode = new Array();
+                oFilterPackageCode.push(oFilter);
+                oFilterPackageCode.push(oFilterCode);
+                var aFilter = new sap.ui.model.Filter({
+                    filters: oFilterPackageCode,
+                    and: false
+                });
+    
+                var oBinding = oEvent.getParameter("itemsBinding");
+                oBinding.filter([aFilter]);
             }
         });
     });
