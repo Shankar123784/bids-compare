fc72bb03a035b0449c02a3fff5e2e10ebe440649
Shankar Idagichadi <shankar.idagichadi@lntinfotech.com>
2023-12-12 11:16:09 +0000

    121223

--- apps-bids/webapp/controller/Bids.controller.js
+++ apps-bids/webapp/controller/Bids.controller.js
@@ -1,15 +1,36 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel",
+	"sap/ui/core/dnd/DragInfo",
+	"sap/ui/core/dnd/DropInfo",
+	"sap/f/dnd/GridDropInfo",
+	"sap/ui/core/library"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, JSONModel, DragInfo, DropInfo, GridDropInfo) {
         "use strict";
 
         return Controller.extend("com.bids.appsbids.controller.Bids", {
             onInit: function () {
                 this.getView().byId("sematicPageId").destroyTitleHeading();
+                // this.byId("list1").setModel(new JSONModel([
+                //     { title: "Open SAP Homepage 2x2", rows: 2, columns: 2 },
+                //     { title: "Your personal information 3x3", rows: 3, columns: 3 },
+                //     { title: "Appointments management 2x4", rows: 2, columns: 4 }
+                // ]));
+    
+                var rowData = [
+                    { "title": "Title 1", "subTitle": "SubTitle 1", "icon": "sap-icon://menu2", "items": [{ "key": "CV1", "text": "CV1"}, { "key": "CV2", "text": "CV2"}, { "key": "CV3", "text": "CV3"},{ "key": "CV4", "text": "CV4"}]},
+                    { "title": "Title 2", "subTitle": "SubTitle 2", "icon": "sap-icon://add-contact", "items": [{ "key": "CV1", "text": "CV1"}, { "key": "CV2", "text": "CV2"}, { "key": "CV3", "text": "CV3"},{ "key": "CV4", "text": "CV4"}]},
+                    { "title": "Title 3", "subTitle": "SubTitle 3", "icon": "sap-icon://business-objects-experience", "items": [{ "key": "CV1", "text": "CV1"}, { "key": "CV2", "text": "CV2"}, { "key": "CV3", "text": "CV3"},{ "key": "CV4", "text": "CV4"}]},
+                    { "title": "Title 4", "subTitle": "SubTitle 4", "icon": "sap-icon://process", "items": [{ "key": "CV1", "text": "CV1"}, { "key": "CV2", "text": "CV2"}, { "key": "CV3", "text": "CV3"},{ "key": "CV4", "text": "CV4"}]},
+                    { "title": "Title 4", "subTitle": "SubTitle 5", "icon": "sap-icon://process", "items": [{ "key": "CV1", "text": "CV1"}, { "key": "CV2", "text": "CV2"}, { "key": "CV3", "text": "CV3"},{ "key": "CV4", "text": "CV4"}]}
+                ];       
+                var oModel = new sap.ui.model.json.JSONModel();
+                oModel.setData({ "cards": rowData });
+                this.getView().setModel(oModel);
             },
             onSearchPackageCode: function (oEvent) {
                 var sValue = oEvent.getParameter("value");
